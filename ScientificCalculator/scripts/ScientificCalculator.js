4bbabc142a48151fb64bc2c7c481a390312ce465
Vasundhara-Tiwari <vasundhara.tiwari_cs19@gla.ac.in>
2021-11-07 22:46:46 +0530

    added js code for calculator

---
+++ ScientificCalculator/scripts/ScientificCalculator.js
@@ -0,0 +1,7 @@
+const inputElement = document.querySelector(".input");
+const outputOperationElement = document.querySelector(".operation .value");
+const outputResultElement = document.querySelector(".result .value");
+
+const OPERATORS = ["+", "-", "*", "/"];
+const POWERS = "POWER(",
+  FACTORIAL = "FACTORIAL";
